6bd687eb2680435b727cf8a1cc6f82feee85f395
Gerardo <gerardo.lucero.c@usach.cl>
2022-05-14 16:07:49 -0400

    archivo prueba exec mio

---
+++ kkexeprueba.c
@@ -0,0 +1,15 @@
+#//include <unistd.h> //Para utilizar fork(), pipes(), entre otros
+#include <stdio.h> //Funciones de entrada y salida como printf
+//#include <stdlib.h> //Asignación de memoria, atoi, etc.
+//#include <string.h> 
+//#include <sys/wait.h> //Define las constantes simbólicas para usar con waitpid(), wait() por ejemplo
+//#include <sys/types.h>
+
+int main(int argc, char* argv[]){
+
+	printf("WENA MANOLO estoy en kkexeprueba.c y toi hablandooooooo\n");
+
+	return(0);
+
+
+}//fin main	
